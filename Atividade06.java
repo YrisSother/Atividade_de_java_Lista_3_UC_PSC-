8c2e3bda65f233042d34ff93d6d6de2d94680a3b
YrisSother <160602963+YrisSother@users.noreply.github.com>
2024-03-21 15:39:55 -0300

    Add files via upload

---
+++ Atividade06.java
@@ -0,0 +1,18 @@
+public class Atividade06 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 20) {
+            System.out.println(num);
+            num++;
+            
+        }
+
+        while (num <= 20) {
+            System.out.println(num);
+            num++;
+            
+        }
+
+    }
+}
---
+++ Atividade06_B.java
@@ -0,0 +1,11 @@
+public class Atividade06_B {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 20) {
+
+            System.out.print(num);
+            num++;
+        }
+    }
+}
