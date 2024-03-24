6abcd13b0e4d66c48c72b83e9ca3733ee786792f
YrisSother <160602963+YrisSother@users.noreply.github.com>
2024-03-24 18:19:03 -0300

    Add files via upload

---
+++ Atividade50.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Atividade50 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de termos (N): ");
+        int n = scanner.nextInt();
+
+        double resultado = calcularH(n);
+
+        System.out.println("O valor de H com " + n + " termos é: " + resultado);
+
+        scanner.close();
+    }
+
+    public static double calcularH(int n) {
+        double h = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            h += 1.0 / i;
+        }
+
+        return h;
+    }
+}
