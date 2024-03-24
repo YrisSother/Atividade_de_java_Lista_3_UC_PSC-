96ddc7905231378a4e1d0e36ef7bfc50c4cc014b
YrisSother <160602963+YrisSother@users.noreply.github.com>
2024-03-24 11:31:29 -0300

    Add files via upload

--- Atividade29.java
+++ Atividade29.java
@@ -3,11 +3,18 @@ public class Atividade29 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n");
+
+        System.out.println("Preço do pão: R$0.18");
+        System.out.println("Panificadora Pão de ontem - Tabela de preços");
+
+        System.out.println("\n");
         
         for (double i = 0; i < 50; i++) {
 
-            double multiplicacao = 1.99 * ( i+1 ) ;
-                        
+            double multiplicacao = 0.18 * ( i+1 ) ;
+                         
             System.out.printf("%.0f - %.2f%n", i + 1, multiplicacao);
          
         }
